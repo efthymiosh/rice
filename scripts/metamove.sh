2f976724ab4cc8297cff104bd4dd29b1bd55d7b9
Efthymios Hadjimichael <Efthymios Hadjimichael>
2015-10-10 19:45:22 +0300

    Add metamove to scripts.

---
+++ scripts/metamove.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#move .mp4 files located in subfolders to main torrent folder
+
+TORR_PATH="/mnt/massivetwo/torrents/"
+SHOWS_PATH="/mnt/massivetwo/TV Series/"
+
+ls "$TORR_PATH"/*/ -d 2> /dev/null | while read FOLDER
+do
+    GOODNAME=`ls "${FOLDER}" -d | sed 's/\(.*\)\/\(.*\)\[.*/\2/g'`
+	OLDPATHNAME=`ls "${FOLDER}" | grep -i ".mp4"`
+	if [[ -f "${FOLDER}${OLDPATHNAME}" ]];
+	then
+		mv "${FOLDER}${OLDPATHNAME}" "$TORR_PATH/${GOODNAME}.mp4"
+	fi
+done
+
+#move .mp4 files from torrents folder to their respective show folders.
+
+ls "$SHOWS_PATH" | while read SHOW
+do
+    NORMALIZED_SHOW=`echo "$SHOW" | sed 's/\ /\./g'`
+	TORRS=`ls "$TORR_PATH"/*.mp4 2> /dev/null | grep -i ${NORMALIZED_SHOW}`
+	if [[ "$?" -eq "0" ]];
+	then
+        DIR="$SHOWS_PATH/$SHOW"
+		if [[ -d "$DIR" ]];
+		then
+			for TORR in $TORRS
+			do
+				mv -v "$TORR" "$DIR"
+			done
+		else
+			ls -d "$DIR"
+			echo "directory $DIR not found for torrents: $TORRS"
+		fi
+	fi
+done
