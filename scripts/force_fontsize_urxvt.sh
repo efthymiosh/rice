2dec6598479aad8344569eedf3386a42b0a6c048
efthymiosh <efthymiosh@di.uoa.gr>
2017-06-13 17:07:18 +0300

    Simplify urxvt font size change

--- scripts/force_fontsize_urxvt.sh
+++ scripts/force_fontsize_urxvt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=$1;
 [[ -z "$1" ]] && size=13
-printf '\33]710;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size:antialias=true:hinting=true"
-printf '\33]711;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size:antialias=true:hinting=true"
-printf '\33]712;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size:antialias=true:hinting=true"
-printf '\33]713;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size:antialias=true:hinting=true"
+printf '\33]710;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size"
+printf '\33]711;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size"
+printf '\33]712;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size"
+printf '\33]713;%s\007' "xft:DejaVu Sans Mono:pixelsize=$size"
