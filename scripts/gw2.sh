5c812ec548d80c01860b06bd9cc917cba4074e8f
efthymiosh <efthymiosh@di.uoa.gr>
2017-07-11 15:58:57 +0300

    Modify special vimrc.

--- scripts/gw2.sh
+++ scripts/gw2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 herbstclient keybind F6 spawn xdotool click 1
-~/Wine/bin/wine /mnt/massiveone/games/Guild\ Wars\ 2/Gw2.exe -dx9single
+~/Wine/bin/wine /media/efthymiosh/Demon/Guild\ Wars\ 2/Gw2.exe -dx9single
 herbstclient keyunbind F6
 
