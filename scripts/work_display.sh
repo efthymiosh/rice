5752e57e9b01fb5fa99e3e87f249d92d1f8c9e0f
Efthymios Hadjimichael <efthymios@withplum.com>
2020-07-18 17:41:53 +0300

    feat(ruggy): Go purple, fix interfaces

--- scripts/work_display.sh
+++ scripts/work_display.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 EXT_HORI=2560
 EXT_VERT=1440
-OUTPUT=DP-1-1
+OUTPUT=HDMI-1
 xrandr --output eDP-1 --pos $(( (${EXT_HORI} - 1920) / 2 ))x${EXT_VERT} --output $OUTPUT --mode ${EXT_HORI}x${EXT_VERT} --pos 0x0
