3f29fbfae130741056041359fac4fe7ed71d847e
Efthymios Hadjimichael <ehadjimichael@id5.io>
2021-09-15 14:47:53 +0300

    Install ansible from ppa

--- provision.sh
+++ provision.sh
@@ -2,7 +2,9 @@
 
 if ! which ansible > /dev/null; then
     sudo apt update
-    sudo apt install -y ansible
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install ansible
 fi
 
 LAPTOP=$1
