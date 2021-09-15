7bf01a09ea6a044fd3f751c5fc995037a9470e17
Efthymios Hadjimichael <ehadjimichael@id5.io>
2021-09-15 16:42:44 +0300

    Revert ppa ansible due to incompatibility of packages

--- provision.sh
+++ provision.sh
@@ -2,9 +2,7 @@
 
 if ! which ansible > /dev/null; then
     sudo apt update
-    sudo apt install -y software-properties-common
-    sudo add-apt-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible
+    sudo apt install -y ansible
 fi
 
 LAPTOP=$1
