081ea01c3aa4ec6baaddc17756028ebc57ce8d0f
Efthymios Hadjimichael <ehadjimichael@id5.io>
2021-09-25 16:13:05 +0300

    Set to full bar when small monitor

--- ansible/roles/polybar/files/panel.sh
+++ ansible/roles/polybar/files/panel.sh
@@ -1,15 +1,22 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+function spawn_polybar {
+    export MONITOR="$(echo $1 | cut -d: -f1)"
+    WIDTH="$(echo $1 | sed 's/^.*:\ \([0-9]\+\)x.*$/\1/')"
+
+    if [ "$WIDTH" -lt 2560 ]; then
+        polybar -q full &
+    else
+        polybar -q leftie &
+        polybar -q rightie &
+    fi
+}
 
 # start polybar on the primary monitor
-export MONITOR=$(polybar -m | grep -ie "primary" | cut -d: -f1)
 export TRAY_POS="right"
-polybar -q leftie &
-polybar -q rightie &
-
+spawn_polybar "$(polybar -m | grep 'primary')"
 
 export TRAY_POS="none"
-for monitor in $(polybar -m | grep -v "primary" | cut -d: -f1) ; do
-    export MONITOR="$monitor"
-    polybar -q leftie &
-    polybar -q rightie &
+polybar -m | grep -v 'primary' | while read ENTRY ; do
+    spawn_polybar "$ENTRY"
 done
