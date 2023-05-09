b9b6500d0ddd349a17f1fc590e503bf43524f000
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-05-09 13:47:48 +0300

    Update nerd fonts, change lsp vim ellipsis char

--- ansible/roles/development/files/neovim/lsp_configs.lua
+++ ansible/roles/development/files/neovim/lsp_configs.lua
@@ -109,9 +109,10 @@ cmp.setup({
   formatting = {
     fields = {'kind', 'abbr', 'menu'},
     format = require('lspkind').cmp_format({
+
       mode = 'symbol', -- show only symbol annotations
       maxwidth = 50, -- prevent the popup from showing more than provided characters
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+      ellipsis_char = '…', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
       before = function(entry, item)
         local menu_icon = {
           nvim_lsp = 'λ',
