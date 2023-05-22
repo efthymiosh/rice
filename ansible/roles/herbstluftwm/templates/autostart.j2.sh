3c572b7e95fcd9f2bd7ff6393a3850675e3f5261
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-05-22 10:51:34 +0300

    Better screenshot file format, also copy image to clipboard

--- ansible/roles/herbstluftwm/templates/autostart.j2.sh
+++ ansible/roles/herbstluftwm/templates/autostart.j2.sh
@@ -48,7 +48,7 @@ herbstclient keybind XF86Search spawn rofi -modi combi -show combi
 herbstclient keybind $Mod-d     spawn rofi -modi combi -show combi
 herbstclient keybind $Mod-Tab   spawn rofi -modi window -show window -kb-row-down 'Super+Tab' -kb-accept-entry '!Super+Tab'
 
-herbstclient keybind Print spawn bash -c 'maim -s ~/screen-$(date +%Y-%b-%d--%H-%M-%S | tr A-Z a-z).png'
+herbstclient keybind Print spawn bash -c 'maim -s | tee ~/screen-$(date +%Y%b%d.%H:%M:%S).png | xclip -t image/png -selection clipboard -i'
 
 herbstclient keybind XF86AudioRaiseVolume spawn amixer -D pulse sset Master 5%+
 herbstclient keybind XF86AudioLowerVolume spawn amixer -D pulse sset Master 5%-
