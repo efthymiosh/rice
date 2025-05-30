7a07daa647edb1cfbec671be2fa712c89a145cac
Efthymios Hadjimichael <f@efhd.dev>
2025-05-30 09:14:35 +0300

    Update sign definition

--- ansible/roles/development/files/neovim/lsp_configs.lua
+++ ansible/roles/development/files/neovim/lsp_configs.lua
@@ -1,4 +1,3 @@
-vim.diagnostic.config({ virtual_text = true })
 require("mason").setup({
   ui = {
     icons = {
--- ansible/roles/development/files/neovim/uiux.lua
+++ ansible/roles/development/files/neovim/uiux.lua
@@ -1,8 +1,26 @@
-local signs = { Error = "✘", Warn = "", Hint = "󰌶", Info = "" }
+local signs = {
+  Error = "✘",
+  Warn = "󱈸",
+  Hint = "󰌶",
+  Info = ""
+}
+
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+vim.diagnostic.config({
+  virtual_text = true,
+  underline = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = signs.Error,
+      [vim.diagnostic.severity.WARN] = signs.Warn,
+      [vim.diagnostic.severity.HINT] = signs.Hint,
+      [vim.diagnostic.severity.INFO] = signs.Info,
+    }
+  }
+})
 
 for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
   vim.api.nvim_set_hl(0, group, {})
