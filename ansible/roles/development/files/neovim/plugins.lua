d03e5b293a913e6ef975d9c1577155a61e15ea33
Efthymios Hadjimichael <efthymiosh@gmail.com>
2025-12-06 12:35:27 +0200

    Do not auto-spawn fn signature

--- ansible/roles/development/files/neovim/plugins.lua
+++ ansible/roles/development/files/neovim/plugins.lua
@@ -72,18 +72,6 @@ require("lazy").setup({
 
     version = '1.*',
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = {
         preset = 'default',
       },
@@ -93,7 +81,6 @@ require("lazy").setup({
         nerd_font_variant = 'mono'
       },
 
-      -- (Default) Only show the documentation popup when manually triggered
       completion = {
         menu = {
           auto_show = false,
@@ -107,8 +94,12 @@ require("lazy").setup({
       },
       signature = {
         enabled = true,
+        trigger = {
+          enabled = false,
+        },
         window = {
           border = 'single',
+          show_documentation = true,
         },
       },
 
