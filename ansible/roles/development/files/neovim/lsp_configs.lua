9bd6c1721d4b3e13ee218059d159f3c5b80f03c4
Efthymios Hadjimichael <efthymiosh@gmail.com>
2026-04-21 16:21:09 +0300

    Fix tree-sitter configuration

--- ansible/roles/development/files/neovim/lsp_configs.lua
+++ ansible/roles/development/files/neovim/lsp_configs.lua
@@ -81,17 +81,13 @@ vim.lsp.config('ruff', {
   }
 })
 
--- treesitter
-require("nvim-treesitter").setup({
-  ensure_installed = "all",
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,
-  }
+-- treesitter (main branch API)
+require("nvim-treesitter").install("stable")
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
 })
 require("illuminate").configure({
   providers = {
